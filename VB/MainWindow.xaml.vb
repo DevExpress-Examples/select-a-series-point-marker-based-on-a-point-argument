d37e01ec12e31353f2bf2440f40a17201a6c6e69
DevExpressExampleBot <fakeEmail>
2025-03-07 14:50:50 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,49 +1,60 @@
-﻿Imports DevExpress.Xpf.Charts
+Imports DevExpress.Xpf.Charts
 Imports System
 Imports System.Collections.Generic
 Imports System.Windows
 Imports System.Windows.Controls
 
 Namespace ArgumentBasedMarkerTypeSelector
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Property DataItems() As New List(Of ChartDataItem)()
-		Public Sub New()
-			InitializeComponent()
-			DataItems.AddRange(GenerateRandomData())
-		End Sub
-		Private Iterator Function GenerateRandomData() As IEnumerable(Of ChartDataItem)
-			Dim random = New Random()
-			For i = 0 To 89
-				Yield New ChartDataItem() With {
-					.DateTimeStamp = DateTime.Today.AddDays(i),
-					.Value = random.Next(0, 100)
-				}
-			Next i
-		End Function
-	End Class
-	Public Class ChartDataItem
-		Public Property DateTimeStamp() As DateTime
-		Public Property Value() As Double
-	End Class
-	Public Class MarkerDataTemplateSelector
-		Inherits DataTemplateSelector
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
-			Dim tempVar As Boolean = TypeOf item Is SeriesPoint
-			Dim seriesPoint As SeriesPoint = If(tempVar, CType(item, SeriesPoint), Nothing)
-			Dim tempVar2 As Boolean = TypeOf container Is FrameworkElement
-			Dim element As FrameworkElement = If(tempVar2, CType(container, FrameworkElement), Nothing)
-			If tempVar2 AndAlso tempVar Then
-				Dim dayOfWeek = seriesPoint.DateTimeArgument.DayOfWeek
-				If dayOfWeek = System.DayOfWeek.Saturday OrElse dayOfWeek = System.DayOfWeek.Sunday Then
-					Return TryCast(element.FindResource("squareModel"), DataTemplate)
-				Else
-					Return TryCast(element.FindResource("circleModel"), DataTemplate)
-				End If
-			End If
-			Return Nothing
-		End Function
-	End Class
+        Public Property DataItems As List(Of ChartDataItem) = New List(Of ChartDataItem)()
+
+        Public Sub New()
+            Me.InitializeComponent()
+            DataItems.AddRange(GenerateRandomData())
+        End Sub
+
+        Private Iterator Function GenerateRandomData() As IEnumerable(Of ChartDataItem)
+            Dim random = New Random()
+            For i = 0 To 90 - 1
+                Yield New ChartDataItem() With {.DateTimeStamp = Date.Today.AddDays(i), .Value = random.Next(0, 100)}
+            Next
+        End Function
+    End Class
+
+    Public Class ChartDataItem
+
+        Public Property DateTimeStamp As Date
+
+        Public Property Value As Double
+    End Class
+
+    Public Class MarkerDataTemplateSelector
+        Inherits DataTemplateSelector
+
+        Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
+            Dim element As FrameworkElement = Nothing, seriesPoint As SeriesPoint = Nothing
+            If CSharpImpl.__Assign(element, TryCast(container, FrameworkElement)) IsNot Nothing AndAlso CSharpImpl.__Assign(seriesPoint, TryCast(item, SeriesPoint)) IsNot Nothing Then
+                Dim dayOfWeek = seriesPoint.DateTimeArgument.DayOfWeek
+                If dayOfWeek = DayOfWeek.Saturday OrElse dayOfWeek = DayOfWeek.Sunday Then
+                    Return TryCast(element.FindResource("squareModel"), DataTemplate)
+                Else
+                    Return TryCast(element.FindResource("circleModel"), DataTemplate)
+                End If
+            End If
+
+            Return Nothing
+        End Function
+
+        Private Class CSharpImpl
+
+            <Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
+            Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
+                target = value
+                Return value
+            End Function
+        End Class
+    End Class
 End Namespace
